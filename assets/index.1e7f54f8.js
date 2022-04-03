75d22896f9a6be2258c4b36fabab2dcec00a31c4
_lmmmmmm <931420654@qq.com>
2022-04-04 00:46:02 +0800

    Deploy: GitHub Page Deploy

---
+++ assets/index.1e7f54f8.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index.ff7dce82.js";import{d as t,o as r,m as s,H as n,u as a}from"./vendor.9ce743dd.js";const p=t({setup(u){const e=o();return(i,c)=>(r(),s("div",null,n(a(e).userConfig.studentName),1))}});export{p as default};
--- assets/index.6adb5a5e.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index.d05adb2a.js";import{d as t,o as r,m as s,H as n,u as a}from"./vendor.9ce743dd.js";const p=t({setup(u){const e=o();return(i,c)=>(r(),s("div",null,n(a(e).userConfig.studentName),1))}});export{p as default};
